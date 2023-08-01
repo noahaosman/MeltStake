0bb0f017e42ee507f68fae6355bc3e298f35edda
noahaosman <noahaosman@gmail.com>
2023-07-31 18:47:53 -0700

    .

--- Leak_detection.py
+++ Leak_detection.py
@@ -14,6 +14,9 @@ leak = DigitalInOut(board.D27)
 leak.direction = Direction.INPUT
 leak.pull = Pull.DOWN
 
+with open('/home/pi/MeltStake/Leak_State.txt', "w") as f:  
+    # write new data line
+    f.write("False")
 
 while leak.value == False:
     time.sleep(0.25)
