f968d07ad5af6902e14654284ff4e22289052379
noahaosman <noahaosman@gmail.com>
2023-09-07 14:09:19 -0700

    added 5th clock speed

--- Clock_Speed.py
+++ Clock_Speed.py
@@ -1,9 +1,10 @@
 # Dictionary of experimentally determined optimal clock speeds to device number
 CLK_SPD = {
-    '00' : 25000000,
+    '00' : 25000000, # needs tuning
     '01' : 23900000,
-    '02' : 24350000,
+    '02' : 24350000, # needs tuning
     '03' : 25000000, # needs tuning
     '04' : 25000000, # needs tuning
+    '05' : 25000000, # needs tuning
 }
 
